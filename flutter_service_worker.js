965e5ffa978c7f2e038163b349101b7713de65cb
Roi Peker <roipeker@gmail.comr>
2022-11-15 17:10:45 -0300

    "commit"

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "947ff982dd0fb8fb64d1ed50b17d1143",
+  "version.json": "99f65b95191fec98fb136a790c4b70fe",
 "splash/img/light-2x.png": "2ee931f29bf3f0b50d8abb1e2ecadade",
 "splash/img/dark-4x.png": "58510241cae7b28b6eae97c373371cee",
 "splash/img/light-3x.png": "bb0d34250890bb54cf8aef4de756da2d",
@@ -16,10 +16,10 @@ const RESOURCES = {
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/style.css": "309d4f23e7374c48a609008fb79e6e99",
 "favicon.ico": "ec49c1978f58531baaff84c3ebe4f7bd",
-"index.html": "19f82c40d4a58a070dd311c9209919db",
-"/": "19f82c40d4a58a070dd311c9209919db",
+"index.html": "502977f39b21e07b2ae6d4a586e021bb",
+"/": "502977f39b21e07b2ae6d4a586e021bb",
 "CNAME": "e4f6958fb51c51a65cc8853ec446b73a",
-"main.dart.js": "1dd64887715c3317bb1fbf5d7784ed13",
+"main.dart.js": "d56e89db7920837cfc1ce4901add86b4",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "f18e2f33f2ca0d363993ba12d1f7f3ae",
 "icons/favicon-16x16.png": "bd3ac64801c75be3c8849fd42b4bb6b7",
